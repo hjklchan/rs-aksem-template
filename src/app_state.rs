94dc0573f813430bdc70327a604f5521b9880f3b
Illusion <1161524405@qq.com>
2024-06-01 00:54:41 +0800

    AppState 的简单实现

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,3 +1,22 @@
+/// Axum 全局状态数据结构
 #[derive(Clone)]
 pub struct AppState {
+    db: (),
+    other: (),
+}
+
+impl AppState {
+    fn new() -> AppState {
+        Self { db: (), other: () }
+    }
+
+    fn with_db(mut self, db: ()) -> Self {
+        self.db = db;
+        self
+    }
+
+    fn with_other(mut self, other: ()) -> Self {
+        self.other = other;
+        self
+    }
 }

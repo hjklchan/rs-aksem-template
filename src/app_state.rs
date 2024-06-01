c1156cd7ebe677e69a32020999d4df1d8dfe3b4d
Illusion <1161524405@qq.com>
2024-06-01 12:06:47 +0800

    初始化数据库连接实例并注册到 AppState

--- src/app_state.rs
+++ src/app_state.rs
@@ -1,9 +1,11 @@
+use sqlx::mysql::MySqlPool;
+
 /// Axum 全局状态数据结构
 #[derive(Clone)]
 pub struct AppState {
-    pub db: (),
+    pub db: MySqlPool,
 }
 
-pub fn new(db: ()) -> AppState {
+pub fn new(db: MySqlPool) -> AppState {
     AppState { db }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // 创建数据库连接池
+    // TODO: database_url 应该通过配置或全局静态配置获取
+    let pool = db::must_connect_pool("mysql://root:@127.0.0.1:3306/test").await;
+
     // 初始化 Axum 全局状态
-    let app_state = Arc::new(app_state::new(()));
+    let app_state = Arc::new(app_state::new(pool));
     // 实例化路由
     let routes = handler::routes(app_state);
     // 实例化 App
@@ -15,6 +19,8 @@ async fn main() -> std::io::Result<()> {
     // Tcp 监听器
     // TODO: addr 应该通过配置或全局静态配置获取
     let tcp_listener = TcpListener::bind("0.0.0.0:8888").await?;
+    // TODO: 应该使用日志库打印
+    println!("Listen on 127.0.0.1:8888");
 
     // 启动服务
     // FIXME: 修复错误

118630ecf010ab4ddb7fe23bc9f22c0f4263540c
Illusion <1161524405@qq.com>
2024-06-01 11:30:28 +0800

    测试错误包

--- src/handler/root.rs
+++ src/handler/root.rs
@@ -1,8 +1,12 @@
 use axum::response::IntoResponse;
 
-use crate::result::OhMyResult;
+use crate::result::{error::ApiError, OhMyResult};
 
+/// Test ok
 pub async fn root_handler() -> impl IntoResponse {
-    let ok: OhMyResult<&str> = Ok("It's ok");
-    ok
+    let is_ok: OhMyResult<()> = Err(ApiError::Database(sqlx::Error::ColumnNotFound(
+        "xmy".into(),
+    )));
+
+    is_ok
 }

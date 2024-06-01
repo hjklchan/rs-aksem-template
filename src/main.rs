609c073c93d3bef9e97d5d6bfd47cacb77a8a511
Illusion <1161524405@qq.com>
2024-06-01 12:30:51 +0800

    创建数据库实例并注册到 AppState 中

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use axum::Router;
 use rs_aksem::{app_state, handler};
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // 创建数据库连接池
     // TODO: database_url 应该通过配置或全局静态配置获取
-    let pool = db::must_connect_pool("mysql://root:@127.0.0.1:3306/test").await;
+    let pool = db::must_connect_pool("mysql://root:mypass@127.0.0.1:3306/test").await;
 
     // 初始化 Axum 全局状态
     let app_state = Arc::new(app_state::new(pool));
@@ -18,9 +18,10 @@ async fn main() -> std::io::Result<()> {
 
     // Tcp 监听器
     // TODO: addr 应该通过配置或全局静态配置获取
-    let tcp_listener = TcpListener::bind("0.0.0.0:8888").await?;
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
+    let tcp_listener = TcpListener::bind(addr).await?;
     // TODO: 应该使用日志库打印
-    println!("Listen on 127.0.0.1:8888");
+    println!("Listen on http://{}", addr.to_string());
 
     // 启动服务
     // FIXME: 修复错误

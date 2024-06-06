be1abf75dcb9f43bdc8d02e33234877f12b205d9
Illusion <1161524405@qq.com>
2024-06-06 21:42:36 +0800

    complete get handler

--- src/handler/tickets/get.rs
+++ src/handler/tickets/get.rs
@@ -1,5 +1,54 @@
-use crate::result::OhMyResult;
+use axum::{
+    extract::{Path, State},
+    Json,
+};
 
-pub async fn get_handler() -> OhMyResult<()> {
-    OhMyResult::Ok(())
+use crate::{
+    app_state::AppState,
+    result::{
+        error::{db::DatabaseError, ticket::TicketError, ApiError},
+        OhMyResult,
+    },
+};
+
+use super::Ticket;
+
+#[derive(Debug, serde::Serialize)]
+pub struct GetTicketRep {
+    item: Ticket,
+}
+
+pub async fn get_handler(
+    State(state): State<AppState>,
+    Path(ticket_id): Path<u64>,
+) -> OhMyResult<Json<GetTicketRep>> {
+    let ticket = sqlx::query_as!(
+        Ticket,
+        r#"
+        SELECT
+            `id`,
+            `assignee_id` as `assignee_id?`,
+            `title` as `title`,
+            `description` as `description?`,
+            `body` as `body?`,
+            `status`,
+            `created_at` as `created_at?`,
+            `updated_at` as `updated_at?`
+        FROM `tickets`
+        WHERE `id` = ? LIMIT 1
+        "#,
+        ticket_id
+    )
+    .fetch_one(&state.db)
+    .await
+    .map_err(|err| match err {
+        sqlx::Error::RowNotFound => {
+            return ApiError::Ticket(TicketError::NotFound);
+        }
+        other_err => {
+            return ApiError::Database(DatabaseError(other_err));
+        }
+    })?;
+
+    Ok(Json(GetTicketRep { item: ticket }))
 }

11da4184f95d66462ab577ebcf95bad556e74a54
Lucas chen <lucas.chen@gbo-asia.com>
2024-06-19 11:13:50 +0800

    simply code

--- src/handler/tickets/delete.rs
+++ src/handler/tickets/delete.rs
@@ -12,17 +12,16 @@ pub async fn delete_handler(
     State(state): State<AppState>,
     Path(ticket_id): Path<u64>,
 ) -> OhMyResult<()> {
-    let sql = "DELETE FROM `tickets` WHERE `id` = ?";
-    let rows_affected = sqlx::query(sql)
+    sqlx::query("DELETE FROM `tickets` WHERE `id` = ?")
         .bind(ticket_id)
         .execute(&state.db)
         .await
-        .map(|result| result.rows_affected())
-        .map_err(|err| DatabaseError(err))?;
-
-    if rows_affected == 0 {
-        return Err(TicketError::NotFound.into());
-    }
-
-    OhMyResult::Ok(())
+        .map(|result| {
+            if result.rows_affected() == 0 {
+                Err(TicketError::NotFound.into())
+            } else {
+                Ok(())
+            }
+        })
+        .map_err(|err| DatabaseError(err))?
 }
